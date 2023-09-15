71b91d558fe3f5800614326de2794501d981c058
zero <squiredd@gmail.com>
2023-09-14 21:54:55 -0700

    display MAC address

---
+++ source/BluetoothLePlayground/ListenerGui/Converters/MacAddressConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace ListenerGui.Converters;
+
+public class MacAddressConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return value;
+        }
+        var num = value as ulong?;
+        if (num == null)
+        {
+            return value;
+        }
+
+        return string.Join(":",
+            BitConverter.GetBytes(num.Value).Reverse()
+                .Select(b => b.ToString("X2"))).Substring(6);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
